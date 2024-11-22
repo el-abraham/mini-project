3110c2009b6070b0eedc80ac3240ee6297b0d2d5
el-abraham <d.abraham1412@gmail.com>
2024-11-22 17:26:34 +0700

    fecth: add migration

---
+++ cmd/migrate/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/el-abraham/mini-project/utils"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+
+	m, err := migrate.New("file://db/migrations", utils.GetEnv("DB_URL", ""))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
+
+	m.Steps(2)
+}
